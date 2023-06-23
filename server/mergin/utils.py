efc29fbf5f875269be76222d33b19f30ef469ff1
Martin Varga <martin.varga@lutraconsulting.co.uk>
2023-06-23 07:55:23 +0200

    Add support to sort by json value

--- server/mergin/utils.py
+++ server/mergin/utils.py
@@ -4,12 +4,12 @@
 from sqlalchemy import Column
 
 
-def parse_order_params(cls, order_params):
+def parse_order_params(cls, order_params, json_sort=None):
     """Parse order parameters in query string
 
     :param cls: Db model class
     :type cls: db.Model
-    :param order_params: order parameter string, e.g. 'name ASC,created DESC'
+    :param order_params: order parameter string, e.g. 'name ASC,created DESC,meta.storage ASC'
     :type order_params: str
 
     :rtype: List[Column]
@@ -20,9 +20,34 @@ def parse_order_params(cls, order_params):
             col, order = p.strip().split(" ")
         except ValueError:
             continue
+        # find candidate for nested json sort
+        if "." in col:
+            col, attr = col.split(".")
+        else:
+            attr = None
         order_attr = cls.__table__.c.get(col, None)
         if not isinstance(order_attr, Column):
             continue
+        # sort by key in JSON field
+        if attr:
+            # unknown field
+            if attr not in json_sort:
+                continue
+
+            from sqlalchemy import JSON
+            if not isinstance(order_attr.type, JSON):
+                continue
+
+            # cast expected type for json key sort
+            # would result in something like "(json_column->>'attribute')::type"))
+            if json_sort[attr] == "int":
+                order_attr = order_attr[attr].as_numeric(20, 0)  # to handle bigint
+            elif json_sort[attr] == "float":
+                order_attr = order_attr[attr].as_float()
+            elif json_sort[attr] == "bool":
+                order_attr = order_attr[attr].as_boolean()
+            else:
+                order_attr = order_attr[attr].as_string()
 
         if order == "ASC":
             order_by_params.append(order_attr.asc())
