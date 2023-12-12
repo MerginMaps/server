c1ca408f8345242ee94845ef99cda9a477da8a1f
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2023-12-12 11:04:20 +0100

    Added back huawei Text update by searching and missing projects

--- web-app/packages/lib/src/modules/project/components/ProjectsTableDataLoader.vue
+++ web-app/packages/lib/src/modules/project/components/ProjectsTableDataLoader.vue
@@ -17,7 +17,13 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
         <div class="flex flex-column align-items-center p-4 text-center">
           <img src="@/assets/map-circle.svg" alt="No projects" />
           <p class="font-semibold p-4">There are currently no projects.</p>
-          <p class="text-sm opacity-80 m-0">You don't have any projects yet.</p>
+          <p class="text-sm opacity-80 m-0">
+            <template v-if="projectsSearch"
+              >We couldn't find any projects matching your search
+              criteria.</template
+            >
+            <template v-else>You don't have any projects yet.</template>
+          </p>
           <template v-if="canCreateProject">
             <p class="text-sm opacity-80 pb-4">Please create new project.</p>
             <PButton @click="newProjectDialog">Create new project</PButton>
@@ -81,7 +87,11 @@ export default defineComponent({
     }
   },
   computed: {
-    ...mapState(useProjectStore, ['projects', 'projectsCount'])
+    ...mapState(useProjectStore, [
+      'projects',
+      'projectsCount',
+      'projectsSearch'
+    ])
   },
   methods: {
     ...mapActions(useProjectStore, ['getProjects']),
