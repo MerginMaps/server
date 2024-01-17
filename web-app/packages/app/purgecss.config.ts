e2066dc2046f06ce66d74c1605d91ba0c42a76b6
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2024-01-17 11:08:44 +0100

    Update purgecss extractor for primeflex classes

--- web-app/packages/app/purgecss.config.ts
+++ web-app/packages/app/purgecss.config.ts
@@ -1,5 +1,11 @@
 module.exports = {
   content: ['./dist/**/*.{html,js}'],
   css: ['./dist/**/index-*.css'],
-  output: './dist/assets/'
+  output: './dist/assets/',
+  extractors: [
+    {
+      extractor: (content) => content.match(/[A-z0-9-:/]+/g) || [],
+      extensions: ['js']
+    }
+  ]
 }
