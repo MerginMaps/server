5e6b675383a34c5b57a4ed6ef4ca87591343b926
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2024-01-17 15:59:25 +0100

    Fix missing button secondary

--- web-app/packages/app/purgecss.config.ts
+++ web-app/packages/app/purgecss.config.ts
@@ -2,7 +2,11 @@ module.exports = {
   content: ['./dist/**/*.{html,js}'],
   css: ['./dist/**/index-*.css'],
   output: './dist/assets/',
-  safelist: [/^lg:col-([0-9]|10|11|12)/, /^col-([0-9]|10|11|12)/],
+  safelist: [
+    /^lg:col-([0-9]|10|11|12)/,
+    /^col-([0-9]|10|11|12)/,
+    /p-button-secondary/
+  ],
   extractors: [
     {
       extractor: (content) => content.match(/[A-z0-9-:/]+/g) || [],
