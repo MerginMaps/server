c8d6f52f15a087d83c5f79221a393794b874bef7
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2024-01-29 08:52:43 +0100

    Update empty text for projects

--- web-app/packages/lib/src/modules/project/components/ProjectsTableDataLoader.vue
+++ web-app/packages/lib/src/modules/project/components/ProjectsTableDataLoader.vue
@@ -15,9 +15,8 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
     >
       <template #empty>
         <div class="flex flex-column align-items-center p-4 text-center">
-          <img src="@/assets/map-circle.svg" alt="No projects" />
-          <p class="font-semibold p-4">There are currently no projects.</p>
-          <p class="text-sm opacity-80 m-0">
+          <img src="@mergin/lib/dist/map-circle.svg" alt="No projects" />
+          <p class="font-semibold m-0 p-4">
             <template v-if="projectsSearch"
               >We couldn't find any projects matching your search
               criteria.</template
@@ -25,7 +24,9 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
             <template v-else>You don't have any projects yet.</template>
           </p>
           <template v-if="canCreateProject">
-            <p class="text-sm opacity-80 pb-4">Please create new project.</p>
+            <p class="text-sm opacity-80 pb-4">
+              Let’s start by creating a first one!
+            </p>
             <PButton @click="newProjectDialog">Create new project</PButton>
           </template>
         </div>
