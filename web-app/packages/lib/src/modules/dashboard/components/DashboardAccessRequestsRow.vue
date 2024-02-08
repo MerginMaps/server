a8db73ab399549ccd52cb81095ca079edc401a87
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2024-02-08 14:24:10 +0100

    Refactor and perf: - do not call /access requests twice - use compoistion api for DashboardAccessRrequestRow

--- web-app/packages/lib/src/modules/dashboard/components/DashboardAccessRequestsRow.vue
+++ web-app/packages/lib/src/modules/dashboard/components/DashboardAccessRequestsRow.vue
@@ -6,54 +6,38 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
 
 <template>
   <app-container
-    v-if="currentNamespace && accessRequests && accessRequestsCount > 0"
+    v-if="
+      userStore.currentWorkspace &&
+      projectStore.accessRequests &&
+      projectStore.accessRequestsCount > 0
+    "
   >
     <app-section>
       <template #title
         >Requests
         <span class="text-color-medium-green"
-          >({{ accessRequestsCount }})</span
+          >({{ projectStore.accessRequestsCount }})</span
         ></template
       >
       <template #default>
-        <slot name="table" :namespace="currentNamespace"></slot>
+        <slot name="table" :namespace="userStore.currentWorkspace.name"></slot>
       </template>
     </app-section>
   </app-container>
 </template>
 
-<script lang="ts">
-import { mapActions, mapState } from 'pinia'
-import { defineComponent } from 'vue'
-
+<script lang="ts" setup>
 import { AppContainer, AppSection } from '@/common/components'
+import { useUserStore } from '@/main'
 import { useProjectStore } from '@/modules/project/store'
 
-export default defineComponent({
-  name: 'DashboardAccessRequestsRow',
-  components: { AppSection, AppContainer },
-  computed: {
-    ...mapState(useProjectStore, [
-      'accessRequests',
-      'accessRequestsCount',
-      'currentNamespace'
-    ])
-  },
-  watch: {
-    currentNamespace: {
-      immediate: true,
-      async handler(value) {
-        if (value) {
-          await this.initNamespaceAccessRequests({
-            namespace: value,
-            params: null
-          })
-        }
-      }
-    }
-  },
-  methods: {
-    ...mapActions(useProjectStore, ['initNamespaceAccessRequests'])
-  }
-})
+const projectStore = useProjectStore()
+const userStore = useUserStore()
+
+if (userStore.currentWorkspace) {
+  projectStore.initNamespaceAccessRequests({
+    namespace: userStore.currentWorkspace.name,
+    params: null
+  })
+}
 </script>
