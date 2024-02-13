f92953d823f682378ee767168ff0e1fc0313d613
Herman Snevajs <herman.snevajs@lutraconsulting.co.uk>
2024-02-13 13:41:55 +0100

    data sanitation before constraint addition

--- server/migrations/community/6bd17a6b8707_registration_date_not_null.py
+++ server/migrations/community/6bd17a6b8707_registration_date_not_null.py
@@ -10,6 +10,7 @@ Create Date: 2024-02-13 13:54:37.632996
 
 """
 from alembic import op
+import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
@@ -20,6 +21,25 @@ depends_on = None
 
 
 def upgrade():
+
+    conn = op.get_bind()
+    # assign some random value to user_profile registration_date field if empty
+    conn.execute(
+        sa.text(
+            """
+    with null_registration_date as (
+        select *
+        from user_profile
+        where registration_date is Null
+        )
+    update user_profile up
+    set registration_date = '2000-01-01 10:00:00.0'
+    from null_registration_date
+    where up.user_id = null_registration_date.user_id
+    """
+        )
+    )
+
     op.alter_column("user_profile", "registration_date", nullable=False)
 
 
