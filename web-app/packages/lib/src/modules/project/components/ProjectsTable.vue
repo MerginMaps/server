2210c790d71aee8f3b1cdae6273fab8b8a560315
Herman Snevajs <herman.snevajs@lutraconsulting.co.uk>
2024-06-18 17:38:30 +0200

    Unify border-radius and paddings with product design

--- web-app/packages/lib/src/modules/project/components/ProjectsTable.vue
+++ web-app/packages/lib/src/modules/project/components/ProjectsTable.vue
@@ -55,7 +55,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
               ><PTag
                 v-if="slotProps.data.access.public && !onlyPublic"
                 severity="success"
-                :pt="{ root: { class: 'p-1 ml-1' } }"
+                :pt="{ root: { class: 'ml-1 pl-0.5 pr-0.5' } }"
                 >Public</PTag
               >
               <i
