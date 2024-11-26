8436f36ed7abaa89fec1ca62262fde53749c23e5
Herman Snevajs <herman.snevajs@lutraconsulting.co.uk>
2024-11-26 11:03:12 +0100

    add missing UsageCards

--- web-app/packages/admin-lib/src/modules/admin/views/OverviewView.vue
+++ web-app/packages/admin-lib/src/modules/admin/views/OverviewView.vue
@@ -5,99 +5,146 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
 -->
 
 <template>
-  <article class="overview">
-    <div class="grid" v-if="usage">
-      <overview-card v-if="usage?.active_monthly_contributors" class="col-12 sm:col-6 lg:col-3">
-        <template #heading>Active monthly contributors</template>
-        <div
-          class="w-full"
-          :style="{
-            height: '40px'
-          }"
+  <admin-layout>
+    <app-container>
+      <app-section ground>
+        <template #header><h1 class="headline-h3">Overview</h1></template>
+      </app-section>
+    </app-container>
+
+    <app-container>
+      <div class="grid" v-if="usage">
+        <usage-card
+          v-if="usage?.active_monthly_contributors"
+          class="col-12 sm:col-6 lg:col-3"
         >
-          <PProgressBar
-            :value="0"
-            :show-value="false"
-          ></PProgressBar>
-        </div>
-        <template #title
-              ><span
-                >{{ this.usage?.active_monthly_contributors[0] }}</span
-              ></template
-            >
-            <template #subtitle>this month</template>
-        <template #footer>
-            <PButton
-              @click=""
-              class="w-full"
-              label="See more"
-            />
+          <template #heading>Active monthly contributors</template>
+          <div
+            class="w-full"
+            :style="{
+              height: '40px'
+            }"
+          >
+            <PProgressBar :value="0" :show-value="false"></PProgressBar>
+          </div>
+          <template #title
+            ><span>{{
+              this.usage?.active_monthly_contributors[0]
+            }}</span></template
+          >
+          <template #subtitle>this month</template>
+          <template #footer>
+            <!--            TODO: click to see last three month graph-->
+            <!--              <PButton-->
+            <!--                @click=""-->
+            <!--                class="w-full"-->
+            <!--                label="See more"-->
+            <!--              />-->
           </template>
-      </overview-card>
-      <overview-card v-if="usage?.storage" class="col-12 sm:col-6 lg:col-3">
-        <template #heading>Used storage</template>
-        <div
-          class="w-full"
-          :style="{
-            height: '40px'
-          }"
-        >
-          <PProgressBar
-            :value="0"
-            :show-value="false"
-          ></PProgressBar>
-        </div>
-        <template #title
-              ><span
-                >{{ this.usage?.storage }}</span
-              ></template
-            >
-      </overview-card>
-      <overview-card v-if="usage?.users" class="col-12 sm:col-6 lg:col-3">
-        <template #heading>Registered accounts</template>
-        <div
-          class="w-full"
-          :style="{
-            height: '40px'
-          }"
-        >
-          <PProgressBar
-            :value="0"
-            :show-value="false"
-          ></PProgressBar>
-        </div>
-        <template #title
-              ><span
-                >{{ this.usage?.users }}</span
-              ></template
-            >
-        <template #footer>
+        </usage-card>
+        <usage-card v-if="usage?.storage" class="col-12 sm:col-6 lg:col-3">
+          <template #heading>Used storage</template>
+          <div
+            class="w-full"
+            :style="{
+              height: '40px'
+            }"
+          >
+            <PProgressBar :value="0" :show-value="false"></PProgressBar>
+          </div>
+          <template #title
+            ><span>{{ this.usage?.storage }}</span></template
+          >
+        </usage-card>
+        <usage-card v-if="usage?.users" class="col-12 sm:col-6 lg:col-3">
+          <template #heading>Registered accounts</template>
+          <div
+            class="w-full"
+            :style="{
+              height: '40px'
+            }"
+          >
+            <PProgressBar :value="0" :show-value="false"></PProgressBar>
+          </div>
+          <template #title
+            ><span>{{ this.usage?.users }}</span></template
+          >
+          <template #footer>
             <PButton
-              @click="$router.push({name: AdminRoutes.PROJECTS})"
+              @click="$router.push({ name: AdminRoutes.ACCOUNTS })"
               class="w-full"
               label="Manage users"
             />
           </template>
-      </overview-card>
-    </div>
-  </article>
+        </usage-card>
+        <usage-card v-if="usage?.projects" class="col-12 sm:col-6 lg:col-3">
+          <template #heading>Projects</template>
+          <div
+            class="w-full"
+            :style="{
+              height: '40px'
+            }"
+          >
+            <PProgressBar :value="0" :show-value="false"></PProgressBar>
+          </div>
+          <template #title
+            ><span>{{ this.usage?.projects }}</span></template
+          >
+          <template #footer>
+            <PButton
+              @click="$router.push({ name: AdminRoutes.PROJECTS })"
+              class="w-full"
+              label="Manage users"
+            />
+          </template>
+        </usage-card>
+        <usage-card v-if="showWorkspaces" class="col-12 sm:col-6 lg:col-3">
+          <template #heading>Workspaces</template>
+          <div
+            class="w-full"
+            :style="{
+              height: '40px'
+            }"
+          >
+            <PProgressBar :value="0" :show-value="false"></PProgressBar>
+          </div>
+          <template #title
+            ><span>{{ this.usage?.workspaces }}</span></template
+          >
+          <template #footer>
+            <PButton
+              @click="$router.push({ name: 'adminWorkspaces' })"
+              class="w-full"
+              label="Manage workspaces"
+            />
+          </template>
+        </usage-card>
+      </div>
+    </app-container>
+  </admin-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { OverviewCard } from "@mergin/lib"
-import {AdminRoutes, useAdminStore} from "@/modules";
-import { mapState } from "pinia";
-import {useRouter} from "vue-router";
+import { AppContainer, AppSection, UsageCard } from '@mergin/lib'
+import { mapState } from 'pinia'
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { AdminRoutes, useAdminStore } from '@/modules'
+import { AdminLayout } from '@/modules/admin/components'
 
 export default defineComponent({
   name: 'OverviewView',
-  components: { OverviewCard },
+  components: { AdminLayout, UsageCard, AppContainer, AppSection },
   computed: {
     AdminRoutes() {
       return AdminRoutes
     },
-    ...mapState(useAdminStore, ['usage', useRouter()]),
+    ...mapState(useAdminStore, ['usage']),
+    showWorkspaces() {
+      const router = useRouter()
+      return router.hasRoute('adminWorkspace') && this.usage?.workspaces
+    }
   },
   methods: {
     getUsage(): any {
@@ -106,12 +153,10 @@ export default defineComponent({
     }
   },
   mounted() {
-    this.getUsage();
+    this.getUsage()
   }
 })
-
 </script>
-
 
 <style lang="scss" scoped>
 .overview {
