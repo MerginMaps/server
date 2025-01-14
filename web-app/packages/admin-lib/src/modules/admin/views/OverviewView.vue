2eb7cf09139ed6754acbf21d3f7ea140c8588381
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2025-01-14 16:53:05 +0100

    Remove monthl contributors card from overview

--- web-app/packages/admin-lib/src/modules/admin/views/OverviewView.vue
+++ web-app/packages/admin-lib/src/modules/admin/views/OverviewView.vue
@@ -31,15 +31,15 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
         <usage-card class="col-12 sm:col-6 lg:col-3">
           <template #heading>Used storage</template>
           <div
-          class="w-full"
-          :style="{
+            class="w-full"
+            :style="{
               height: '40px'
             }"
           >
             <PProgressBar :value="0" :show-value="false"></PProgressBar>
           </div>
           <template #title
-          ><span>{{
+            ><span>{{
               $filters.filesize(usage.storage, null, 0)
             }}</span></template
           >
@@ -85,21 +85,6 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
               label="Manage projects"
             />
           </template>
-        </usage-card>
-        <usage-card class="col-12 sm:col-6 lg:col-3">
-          <template #heading>Active monthly contributors</template>
-          <div
-            class="w-full"
-            :style="{
-              height: '40px'
-            }"
-          >
-            <PProgressBar :value="0" :show-value="false"></PProgressBar>
-          </div>
-          <template #title
-            ><span>{{ usage?.active_monthly_contributors }}</span></template
-          >
-          <template #subtitle>this month</template>
         </usage-card>
         <usage-card v-if="showWorkspaces" class="col-12 sm:col-6 lg:col-3">
           <template #heading>Workspaces</template>
