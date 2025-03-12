61e16a7946c8cb1cf1c755a8dbdbb5dee8fb5042
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2025-03-12 15:19:32 +0100

    Added severity option to TipMessage - new color negative-light-color - add message option to confirm dialog to show TipMessage with props - make confirm dialog more dense
    Fixed:
    - remove tip message from DeleteAccountDialog

--- web-app/packages/lib/src/common/components/TipMessage.vue
+++ web-app/packages/lib/src/common/components/TipMessage.vue
@@ -7,30 +7,71 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
 <template>
   <!-- Tip from MM -->
   <div
-    class="tip-message flex flex-column row-gap-2 md:flex-row align-items-center border-round-xl p-4"
+    class="tip-message flex flex-column md:flex-row align-items-center border-round-xl p-4"
+    :class="[severityClass]"
   >
-    <div class="md:mr-4">
-      <slot name="image">
-        <img src="@/assets/bulb.svg" aria-label="Bulb" />
-      </slot>
-    </div>
+    <slot name="image">
+      <img
+        v-if="severity === 'info'"
+        width="50"
+        height="50"
+        src="@/assets/bulb.svg"
+        aria-label="Bulb"
+      />
+      <img
+        v-if="severity === 'danger'"
+        width="50"
+        height="50"
+        src="@/assets/exclamation.svg"
+        aria-label="Exclamation"
+      />
+    </slot>
 
-    <div class="text-center md:text-left">
-      <p class="tip-message-title title-t3 mb-1">
+    <div class="md:pl-4 text-center md:text-left">
+      <p :class="['tip-message-title title-t3']">
         <slot name="title">Tip from Mergin Maps</slot>
       </p>
-      <p class="opacity-80 paragraph-p5">
+      <p class="opacity-80 paragraph-p6">
         <slot name="description"></slot>
       </p>
     </div>
   </div>
 </template>
 
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { TipMessageProps } from './types'
+
+type Severity = 'info' | 'danger'
+
+const props = withDefaults(defineProps<TipMessageProps>(), {
+  severity: 'info'
+})
+
+const severityClass = computed(() => {
+  const mapper: Record<Severity, string> = {
+    info: 'tip-message-info',
+    danger: 'tip-message-danger'
+  }
+  return mapper[props.severity]
+})
+</script>
+
 <style scoped lang="scss">
 .tip-message {
-  background-color: var(--light-green-color);
-  &-title {
-    color: var(--deep-ocean-color);
+  &.tip-message-info {
+    background-color: var(--light-green-color);
+    .tip-message-title {
+      color: var(--deep-ocean-color);
+    }
+  }
+
+  &.tip-message-danger {
+    background-color: var(--negative-light-color);
+    .tip-message-title {
+      color: var(--grape-color);
+    }
   }
 }
 </style>
--- web-app/packages/lib/src/modules/user/components/DeleteAccountConfirm.vue
+++ web-app/packages/lib/src/modules/user/components/DeleteAccountConfirm.vue
@@ -17,22 +17,10 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
     }"
     severity="danger"
     @confirm="confirm"
-  >
-    <TipMessage>
-      <template #image
-        ><img wdth="50" height="50" src="@/assets/exclamation.svg"
-      /></template>
-      <template #title>Please note</template>
-      <template #description
-        >In order to delete your account, enter your username in the field above
-        and click Yes.</template
-      >
-    </TipMessage>
-  </ConfirmDialog>
+  />
 </template>
 
 <script setup lang="ts">
-import TipMessage from '@/common/components/TipMessage.vue'
 import { DeleteAccountConfirmProps } from '@/modules'
 import ConfirmDialog from '@/modules/dialog/components/ConfirmDialog.vue'
 
