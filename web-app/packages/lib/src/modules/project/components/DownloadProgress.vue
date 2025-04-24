ede4e3c201c9d4022e76af595aa72226b8b188b7
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2025-04-24 17:04:41 +0200

    Cleanup of download progress

--- web-app/packages/lib/src/modules/project/components/DownloadProgress.vue
+++ web-app/packages/lib/src/modules/project/components/DownloadProgress.vue
@@ -13,6 +13,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
 </template>
 
 <script lang="ts">
+import { mapActions, mapState } from 'pinia'
 import { defineComponent } from 'vue'
 
 import { useProjectStore } from '@/modules/project/store'
@@ -20,27 +21,26 @@ import { useProjectStore } from '@/modules/project/store'
 // we need to use options api here , because useToast is not properly handled multiple toast services
 export default defineComponent({
   name: 'DownloadProgress',
-  data() {
-    return {
-      projectStore: useProjectStore()
-    }
+  computed: {
+    ...mapState(useProjectStore, ['project', 'projectDownloading'])
   },
   methods: {
+    ...mapActions(useProjectStore, ['cancelDownloadArchive']),
     open() {
       this.$toast.add({
         group: 'download-progress',
         severity: 'info',
-        summary: 'Downloading project archive',
+        summary: `Downloading ${this.project?.name}`,
         detail: 'Please wait while your project is being downloaded.',
         life: undefined
       })
     },
     close() {
-      this.projectStore.cancelDownloadArchive()
+      this.cancelDownloadArchive()
     }
   },
   watch: {
-    'projectStore.projectDownloading'(value) {
+    projectDownloading(value) {
       if (value) {
         this.open()
       } else {
