626e6cb3c23116ca06a89afb8be4174bf5e95916
Herman Snevajs <herman.snevajs@lutraconsulting.co.uk>
2025-07-29 00:36:24 +0200

    test statistics, test check email

--- server/mergin/tests/test_cli.py
+++ server/mergin/tests/test_cli.py
@@ -1,10 +1,14 @@
 # Copyright (C) Lutra Consulting Limited
 #
 # SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
+
 import shutil
 
 import pytest
 import os
+from unittest.mock import patch
+
+# from flask import current_app
 
 from mergin.auth.models import User
 from mergin.sync.models import Project, ProjectVersion
@@ -16,6 +20,7 @@ from mergin.tests import (
     test_project_dir,
 )
 from mergin.sync.config import Configuration as sync_config
+
 
 test_create_project_data = [
     # missing arguments
@@ -212,3 +217,80 @@ def test_remove_project(runner, project_name, code, output):
         ).first()
         assert project.removed_at
         assert not project.removed_by
+
+
+@pytest.mark.parametrize("collect_stats", [True, False])
+@patch("mergin.stats.tasks.send_statistics.delay")
+@patch("mergin.stats.tasks.save_statistics.delay")
+@patch("mergin.commands._check_celery")
+@patch("mergin.commands._echo_title")
+def test_send_statistics(
+    mock_echo_title,
+    mock_check_celery,
+    mock_save_statistics,
+    mock_send_statistics,
+    collect_stats,
+    app,
+):
+    """Test send statistics helper"""
+    from mergin.commands import _send_statistics
+
+    mock_echo_title.return_value = ""
+    mock_check_celery.return_value = True
+    mock_save_statistics.return_value = None
+    mock_send_statistics.return_value = None
+    app.config["COLLECT_STATISTICS"] = 1 if collect_stats else 0
+
+    _send_statistics(app)
+    assert mock_save_statistics.call_count == 1
+    assert mock_send_statistics.call_count == (1 if collect_stats else 0)
+
+
+test_check_email_data = [
+    (0, 1, 1, 1, "Email sent."),  # success
+    (0, 1, 1, 0, ""),  # celery is not running
+    (
+        1,
+        1,
+        1,
+        1,
+        "Sending emails is disabled. Please set MAIL_SUPPRESS_SEND=False to enable sending emails.",
+    ),  # emails are suppressed
+    (0, 0, 1, 1, "Error: Missing option '--email'"),  # email parameter is missing
+    (0, 1, 0, 1, "No default sender set."),  # default sender is missing
+]
+
+
+@patch("mergin.celery.send_email_async.delay")
+@patch("mergin.commands._check_celery")
+@pytest.mark.parametrize(
+    "suppressed,recipient,sender,celery_check,output", test_check_email_data
+)
+def test_check_email(
+    mock_check_celery,
+    mock_send_email,
+    suppressed,
+    recipient,
+    sender,
+    celery_check,
+    output,
+    runner,
+    app,
+):
+    """Test check_email command"""
+    mock_send_email.return_value = None
+    mock_check_celery.return_value = celery_check
+    app.config["MAIL_SUPPRESS_SEND"] = suppressed
+    app.config["MAIL_DEFAULT_SENDER"] = "sender@mergin.com" if sender else None
+
+    result = runner.invoke(
+        args=[
+            "server",
+            "send-check-email",
+            "--email",
+            f" test@mergin.com " if recipient else None,
+        ]
+    )
+    assert output in result.output
+    sent = 1 if output == "Email sent." else 0
+    assert mock_send_email.call_count == sent
