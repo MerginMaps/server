ed6778705e9038cd7a927b5f8ae4032ba8ca083f
Herman Snevajs <herman.snevajs@lutraconsulting.co.uk>
2026-01-09 15:30:17 +0100

    introduce user card component

---
+++ web-app/packages/lib/src/common/components/UserSummary.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex align-items-center w-full">
+    <PAvatar
+      :label="$filters.getAvatar(email, fullName)"
+      size="large"
+      shape="circle"
+      :pt="{
+        root: {
+          class: 'mr-2 text-color-forest font-semibold flex-shrink-0',
+          style: { borderRadius: '50%' }
+        }
+      }"
+    />
+
+    <div class="flex flex-column justify-content-between">
+      <p class="title-t4 overflow-wrap-anywhere">
+        {{ fullName ? `${fullName} (${username})` : username }}
+      </p>
+
+      <p class="paragraph-p6 overflow-wrap-anywhere font-normal">
+        {{ email }}
+        <template v-if="isMe"> (me)</template>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  username: string
+  email: string
+  fullName?: string
+  isMe?: boolean
+}
+
+defineProps<Props>()
+</script>
